d7cf40d7819ef3bd3afdce0c5717bc153701563a
ankit das <ankit.das2019@sait.ac.in>
2022-10-28 13:05:37 +0530

    basic string, for loop, printInJavaScript

---
+++ WorkOnString.js
@@ -0,0 +1,16 @@
+let s = 'hello';
+console.log(s[1]);
+
+//toLowerCase()
+let j = "heyyYY";
+console.log(j.toLowerCase());
+
+//toUpperCase()
+let k = "HELLo";
+console.log(k.toUpperCase());
+
+
+// trim() function
+let l= "    Ankit  ";
+console.log(l)
+console.log(l.trim());
---
+++ printInJavaScript.js
@@ -0,0 +1,27 @@
+console.log("hello I am ankit .....");
+
+var i = 98;
+console.log(i);
+
+let k =980;
+console.log(k);
+
+var i =90;
+console.log(i);
+
+const p =5;
+console.log(5);
+
+const q =983;
+console.log(i);
+
+// const i=98;
+// console.log(i);
+
+//YOU CAN use to $ for variable declaration
+var $M= "ankit";
+console.log($M);
+
+var l= "ankit";
+console.log(`heey my name is ${l}`);
+
